8a00cd120643b6c533d3212aa34952ba062a3479
Bhargavi <82858718+Bhargavi-hash@users.noreply.github.com>
2021-10-23 22:13:32 +0530

    done :)

--- Finance.py
+++ Finance.py
@@ -1,13 +1,25 @@
+import subprocess as sp
+import pymysql
+import pymysql.cursors
+import sys
+from main import *
 from Clear import *
-from ViewTable import *
+from viewTable import *
+from colorama import Fore, Style
+from simple_colors import *
+from tabulate import tabulate
+from os import system, name
+from time import sleep
 
-def Finance():
+#*************************************** Finance analysis ************************************************************
+
+def Finance(con, cur):
     clear()     # Clearing screen before displaying further options
     
     # Displaying options to choose
 
     print(blue("--------------------------------------------------------------------------", 'bold'))
-    print(red("\t\t     Choose onr from below to analyze", 'bold'))
+    print(red("\t\t     Choose one from below to analyze", 'bold'))
     print(blue("--------------------------------------------------------------------------\n", 'bold'))
     
     print(green("--> 1.  Average monthly rent paid by the Franchise", 'bold'))
@@ -64,63 +76,3 @@ def Finance():
 
 
 
-#******************************************** Program starts here ************************************************************
-
-while(1):
-    tmp = sp.call('clear', shell=True)
-    username = input(green("Username: ", 'bold'))
-    password = input(green("Password: ", 'bold'))
-
-    try:
-        con = pymysql.connect(host='localhost',
-                              user=username,
-                              password=password,
-                              db='Franchise',
-                              cursorclass=pymysql.cursors.DictCursor)
-    except Exception as e:
-        print(e)
-        tmp = sp.call('clear', shell=True)
-        print(red('Connection Failed:', 'bold'))
-        print(Fore.RED + "Credentials entered are incorrect or user doesn't have access to database")
-        tmp = input(Fore.GREEN + "Enter any key to CONTINUE > ")
-        print(Style.RESET_ALL)
-        continue
-
-    with con:
-        cur = con.cursor()
-        exitflag = 0
-        while(1):
-            tmp = sp.call('clear', shell=True)
-            # refreshDatabase()
-            print(blue("----------- CHOOSE AN OPTION -----------\n", 'bold'))
-            print(yellow("\t--> 1.  Display Options", 'bold'))
-            print(yellow("\t--> 2.  Addition Options", 'bold'))
-            print(yellow("\t--> 3.  Deletion Options", 'bold'))
-            print(yellow("\t--> 4.  Modify/Update Options", 'bold'))
-            print(yellow("\t--> 5.  Search Options", 'bold'))
-            print(yellow("\t--> 6.  Finance Analysis Options", 'bold'))
-            print(yellow("\t--> 7.  Quit", 'bold'))
-            
-            inp = input(cyan("\nCHOICE ? ", 'bold'))
-            
-            if(inp == '1'):
-                Display()
-            # elif(inp == '2'):
-            #     addOptions()
-            # elif(inp == '3'):
-            #     deleteOptions()
-            elif(inp == '4'):
-                Update()
-            elif(inp == '5'):
-                Search()
-            elif(inp == '6'):
-                Finance()
-            #         exitflag = 1
-            #         print("Bye")
-            #         break
-            
-            print("Press enter to continue ... ")
-            x=input()
-
-    if exitflag == 1:
-        break
